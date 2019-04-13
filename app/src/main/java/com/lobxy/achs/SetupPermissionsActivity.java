408901b4313f440388d33130dcc05c52281cd3bd
Lovish Pandey <Lovish.ku@gmail.com>
2019-04-13 20:23:27 +0530

    Croping Image

---
+++ app/src/main/java/com/lobxy/achs/SetupPermissionsActivity.java
@@ -0,0 +1,17 @@
+package com.lobxy.achs;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SetupPermissionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup_permissions);
+    }
+
+    //Get permissions from user.
+
+
+}
